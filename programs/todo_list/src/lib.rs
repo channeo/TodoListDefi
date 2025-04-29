760bfa5064d2390daf8eea979c0b61403100e7e9
badien <Vubadien2172004@gmail.com>
2025-04-29 10:05:48 +0700

    update code

--- programs/todo_list/src/lib.rs
+++ programs/todo_list/src/lib.rs
@@ -1,16 +1,17 @@
-use anchor_lang::prelude::*;
+// use anchor_lang::prelude::*;
 
-declare_id!("ArjFD4yXdGkcKaSeYEtvya9BcyoncH2TcPhSeJLxKAz1");
+// declare_id!("ArjFD4yXdGkcKaSeYEtvya9BcyoncH2TcPhSeJLxKAz1");
 
-#[program]
-pub mod todo_list {
-    use super::*;
+// #[program]
+// pub mod todo_list {
+//     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-}
+//     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+//         msg!("Greetings from: {:?}", ctx.program_id);
+//         Ok(())
+//     }
+// }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+// #[derive(Accounts)]
+// pub struct Initialize {}
+pub mod schema;
---
+++ programs/todo_list/src/schema/mod.rs
@@ -0,0 +1,5 @@
+
+
+pub mod userData;
+pub mod projectData;
+pub mod taskData;
